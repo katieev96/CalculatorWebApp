95c78b3aab86428604ab4bd07ad3c4026fabbcf1
Katie Elizabeth Vick <10608995@live.mercer.edu>
2020-09-13 16:58:34 -0400

    Add project files.

---
+++ CalculatorWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CalculatorWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
